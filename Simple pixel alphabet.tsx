c7284f561a71283f27748d676f11c02f974bd32a
Riton69 <129973355+Riton69@users.noreply.github.com>
2023-04-17 21:46:28 +0200

    new tilesets

---
+++ Simple pixel alphabet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Simple pixel alphabet" tilewidth="32" tileheight="32" tilecount="2" columns="2">
+ <image source="tilesets/Simple pixel alphabet.png" width="80" height="48"/>
+</tileset>
